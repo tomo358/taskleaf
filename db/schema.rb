0ecf61c1370801a5a2c536be3477d9c56e94fb03
358 tomo <5cho38romu@gmail.com>
2019-07-03 19:22:36 +0900

    chapter_3 finish

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20190624122618) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
-    t.text "discription"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
