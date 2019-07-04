85b52acc2a5fa2c661644681ef2540af88f2eb4a
358 tomo <5cho38romu@gmail.com>
2019-07-04 20:06:57 +0900

    chapter_4-4 finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190624122618) do
+ActiveRecord::Schema.define(version: 20190703112852) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
