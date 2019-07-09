833b880165d9103ee187c7fc3f9641981b9d0d7b
358 tomo <5cho38romu@gmail.com>
2019-07-09 20:43:03 +0900

    Implement login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_method :current_user
+  helper_method :current_user
+
+  private
+
+  def login_required
+    redirect_to login_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
